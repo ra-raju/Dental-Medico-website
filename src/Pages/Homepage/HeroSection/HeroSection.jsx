e80698cc650dfbb4a54a724b2a3f5e94df689664
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-10-19 15:00:13 +0600

    slider size updaate

--- src/Pages/Homepage/HeroSection/HeroSection.jsx
+++ src/Pages/Homepage/HeroSection/HeroSection.jsx
@@ -6,37 +6,37 @@ const HeroSection = () => {
   return (
     <div className="hero-section">
       <Carousel fade className="slider">
-        <Carousel.Item>
+        <Carousel.Item className="slider-item">
           <img
             className="d-block w-100 slider-img"
             src="./image/slide1-min.jpg"
             alt="First slide"
           />
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="slider-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 slider-img"
             src="./image/slide2-min.jpg"
             alt="First slide"
           />
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="slider-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 slider-img"
             src="./image/slide3-min.jpg"
             alt="First slide"
           />
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="slider-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 slider-img"
             src="./image/slide4-min.jpg"
             alt="Second slide"
           />
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="slider-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 slider-img"
             src="./image/slide5-min.jpg"
             alt="Third slide"
           />
